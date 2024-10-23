0d757631b53882d20c631978627fb21fa942bdcd
Iryna Mariiko <IrynaMM497@gmail.com>
2024-10-23 15:36:29 +0300

    make build

---
+++ index.61519e4a.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("form input").forEach(function(e){var t=document.createElement("label");t.className="field-lable",t.setAttribute("for",e.id);var r=e.name.toUpperCase();t.textContent=r,e.setAttribute("placeholder",r),e.parentNode.insertBefore(t,e)});
+//# sourceMappingURL=index.61519e4a.js.map
